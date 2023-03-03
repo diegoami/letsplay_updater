1964800108da129aa135b9577fd5826f06292c0d
Diego Amicabile <diego.amicabile@gmail.com>
2023-03-03 21:13:25 +0100

    Update thumbnails

--- generate_thumbnails.py
+++ generate_thumbnails.py
@@ -4,6 +4,27 @@ import os
 import yaml
 import time
 from PIL import Image, ImageFont, ImageDraw
+
+def split_string(s):
+    words = s.split()
+    n = len(words)
+    half_n = round(n / 2)
+    index = 0
+    total = 0
+    for i, word in enumerate(words):
+        total += len(word)
+        if total >= half_n:
+            index = i
+            break
+    return ' '.join(words[:index+1]), ' '.join(words[index+1:])
+
+# Example usage
+s = "This is a sample string that we want to split into two halves without breaking any words."
+left, right = split_string(s)
+print(left)
+print(right)
+
+
 def generate_thumbnails(youtube, playlist_id, directory, skipped, max, thumbnail_name, remove_texts, text_conf):
     index = 0
     font1 = ImageFont.truetype(text_conf["font"]["name"], text_conf["font"]["size"])
@@ -27,12 +48,19 @@ def generate_thumbnails(youtube, playlist_id, directory, skipped, max, thumbnail
                 if remove_texts:
                     for remove_text in remove_texts:
                         extracted_title = extracted_title.replace(remove_text, '')
+
                 extracted_title = extracted_title.replace('-', '').replace('(', '').replace(')', '').replace(',', '').strip()
 
                 baseImg = Image.open(thumbnail_file_input)
                 img = baseImg.copy()
                 imgDraw = ImageDraw.Draw(img)
-                imgDraw.text((text_conf["coords"]["x2"], text_conf["coords"]["y"]), extracted_title, color, font1)
+                if "y2" in text_conf["coords"]:
+                    ext_title1, ext_title2 = split_string(extracted_title)
+                    imgDraw.text((text_conf["coords"]["x2"], text_conf["coords"]["y"]), ext_title1, color, font1)
+                    imgDraw.text((text_conf["coords"]["x2"], text_conf["coords"]["y2"]), ext_title2, color, font1)
+                else:
+                    imgDraw.text((text_conf["coords"]["x2"], text_conf["coords"]["y"]), extracted_title, color, font1)
+                ext_title1, ext_title2 = split_string(extracted_title)
                 #youtube.upload_thumbnail(video_id, thumbnail_file)
                 img = img.resize((1280, 720))
                 img.save(thumbnail_file_output)
